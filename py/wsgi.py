ef95ea23d53ef372f97b3a2ab763fad17861c2c3
hongbin <fusis1@naver.com>
2018-11-01 00:33:16 +0900

    python 3.6.2 가상환경 적용

--- py/wsgi.py
+++ py/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gazua project.
+WSGI config for py project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gazua.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'py.settings')
 
 application = get_wsgi_application()
