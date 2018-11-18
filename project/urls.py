a9f71f600c616144d3050c0747d57f92b7b554a2
khb1109 <fusis1@naver.com>
2018-11-18 15:48:20 +0900

    restart

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""py URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
